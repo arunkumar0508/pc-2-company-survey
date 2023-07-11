9c61c506e444423c787a5591b96d18d6da30e6d3
ak8a66 <ak8a66@gmail.com>
2023-07-11 19:27:35 +0530

    done

--- src/main/java/com/jap/AverageAge.java
+++ src/main/java/com/jap/AverageAge.java
@@ -8,9 +8,18 @@ public class AverageAge {
 
         try{
             //Write the logic
-
+                int sum=0;
+                int count=0;
+                for(String age: ageRawData){
+                    int ageValue= Integer.parseInt(age);
+                            sum += ageValue;
+                            count++;
+                }
+                if(count>0){
+                    avg= (double)sum/count;
+                }
             //Handle specific exception
-        }catch (Exception exception)
+        }catch (NumberFormatException exception)
         {
             return exception.toString();
         }
